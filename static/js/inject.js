ea218363550dccd418cc214ebc2cf369879a0c2f
sikhism-dot <sikhism@ya.ru>
2026-04-27 09:27:39 +0300

    Update inject.js

--- static/js/inject.js
+++ static/js/inject.js
@@ -5,3 +5,22 @@ for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){retur
 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}
 )(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
 ym(108768935,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});
+
+
+// VideoRoll — Под текстом статьи, перед похожими
+(function(){
+    var target = document.querySelector('.article-body');
+    if (!target) return;
+
+    // Ad container
+    var wrap = document.createElement('div');
+    wrap.style.cssText = 'margin:20px 0;text-align:center';
+    wrap.innerHTML = '<div id="vid_vpaut_div" style="display:inline-block;width:600px;height:320px" vid_vpaut_pl="42025"></div>';
+    target.parentNode.insertBefore(wrap, target.nextSibling);
+
+    // External script
+    var s = document.createElement('script');
+    s.src = 'https://videoroll.net/js/vid_vpaut_script.js';
+    s.async = true;
+    document.body.appendChild(s);
+})();
